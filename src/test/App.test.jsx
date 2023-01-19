71e56e83047274f9590f2b298ca42645eac5a9c4
unknown <sidduinevitable@gmail.com>
2023-01-19 20:50:58 +0530

    add base project to test github actions

---
+++ src/test/App.test.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../App";
+
+test("basic test", () => {
+  const component = renderer.create(<App />);
+  expect(component.toJSON()).toMatchSnapshot();
+});
